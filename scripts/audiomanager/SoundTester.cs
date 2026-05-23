9a7dab07f7944bc9a01e871cb5d6ed64cf1adeea
DiscountGrocery <austinmnewby@gmail.com>
2026-05-23 16:32:20 -0400

    More Import

--- scripts/audiomanager/SoundTester.cs
+++ scripts/audiomanager/SoundTester.cs
@@ -3,11 +3,20 @@ using System;
 
 public partial class SoundTester : Node
 {
+	[Export] AudioStreamPlayer player;
     public override void _UnhandledInput(InputEvent @event)
     {
         if (Input.IsActionJustPressed("fire"))
 		{
-			AudioManager.instance.PlaySFX();
+			AudioManager.instance.PlaySFX("gun-spray-start");
+			AudioManager.instance.PlaySFX("gun-spray-loop");
+			AudioManager.instance.CancelSFX("gun-spray-end");
+		}
+        if (Input.IsActionJustReleased("fire"))
+		{
+			AudioManager.instance.PlaySFX("gun-spray-end");
+			AudioManager.instance.CancelSFX("gun-spray-start");
+			AudioManager.instance.CancelSFX("gun-spray-loop");
 		}
 	}
 }
