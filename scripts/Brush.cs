a3fea8cb380047ba5f5400c0714dc539f48a8d22
jdugan35 <jdugan35@gatech.edu>
2026-05-23 12:13:00 -0400

    dirt blob

---
+++ scripts/Brush.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+public partial class Brush : Node3D
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta)
+    {
+        // GD.Print(GlobalTransform.Basis.Z);
+        if (Input.IsActionPressed("fire"))
+        {
+            var spaceState = GetWorld3D().DirectSpaceState;
+            var origin = GlobalTransform.Origin;
+            var direction = -GlobalTransform.Basis.Z;
+
+            var query = PhysicsRayQueryParameters3D.Create(origin, origin + direction * 1000f);
+            var result = spaceState.IntersectRay(query);
+
+            if (result.Count > 0)
+            {
+                var collider = result["collider"].As<Node>();
+                if (collider != null && collider.IsInGroup("dirt"))
+                {
+                    GD.Print("Hit dirt at ", result["position"]);
+                }
+            }
+        }
+    }
+}
