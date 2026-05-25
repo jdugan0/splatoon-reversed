f20283fdaae5b51f95f0489d828e680e34d85496
jdugan35 <jdugan35@gatech.edu>
2026-05-25 12:35:18 -0400

    small fix

--- scripts/DirtyWall.cs
+++ scripts/DirtyWall.cs
@@ -46,8 +46,11 @@ public partial class DirtyWall : MeshInstance3D
         SplatType.BRUSH.Brush = Brush;
         SplatType.WALK.Brush = WalkSplat;
         SplatType.JUMP.Brush = JumpSplat;
-        Vector3 a = parentBox.GetAabb().Size;
-        Mesh.Set("size", new Vector3(a.X, a.Z, 0.01f));
+        if (parentBox != null)
+        {
+            Vector3 a = parentBox.GetAabb().Size;
+            Mesh.Set("size", new Vector3(a.X, a.Z, 0.01f));
+        }
         planeSize = Mesh.Get("size").As<Vector2>();
         collisionShape.Shape.Set("size", new Vector3(planeSize.X, 0, planeSize.Y));
         Mask.Size = new Vector2I(
